f6966a595d34918018d65f2c2057fa523dc7b403
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-09 17:16:54 +0900

    Create_Association_To_EachModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :groups, through: :members
+  has_many :members
+  has_many :messages
 end
