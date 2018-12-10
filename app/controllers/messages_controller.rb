4e88a09285c21e30c9bc52a53cf0adb5add7f675
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-10 18:53:24 +0900

    Carrierwave_Setting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  mount_uploader :image, ImageUploader
   def index
     @group = current_user.groups
     @groop_now = Group.find(params[:group_id])
