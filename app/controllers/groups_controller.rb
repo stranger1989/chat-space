1c6489426a1e71a3323416711825cdbc703dd36e
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-10 17:55:22 +0900

    Add_Group_Edit_function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
     @group = current_user.groups
   end
@@ -21,6 +23,7 @@ class GroupsController < ApplicationController
   end
 
   def update
+    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
@@ -31,6 +34,10 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, { user_ids: [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
   def index
+    @group = current_user.groups
+    @groop_now = Group.find(params[:group_id])
   end
 
   def create
