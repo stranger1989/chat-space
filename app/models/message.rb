83d1d8b1b7970376ffc2519cf00c1d9503319d03
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-11 15:52:46 +0900

    Add_MessageModel_Validates

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   belongs_to :group
   belongs_to :user
 
-  validates :content, presence: true, unless: :image?
+  validates :body, presence: true
+  validates :image, presence: true, unless: :image?
 end
