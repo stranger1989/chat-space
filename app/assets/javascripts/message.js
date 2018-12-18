cbb16fe5dd10d20c31a7c1f1a0d582dd6b8d8c5b
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-18 19:25:49 +0900

    Add_Chatspace_AutoUpdate

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function() {
   function buildHTML(message){
     var image_tag = `<img src= "${ message.image['url'] }" />`
-    var html = `<div class='main-contents__chat-list__text-box'>
+    var html = `<div class='main-contents__chat-list__text-box chat data-id=${message.id}'>
                 <h4>${ message.name }</h4>
                 <div class="post_time"> ${ message.date } </div>
                 <div class="post_time">${ message.time }</div>
@@ -10,14 +10,12 @@ $(function() {
                 </div>`
     return html;
   }
-
   $(".new_message").submit(function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $(this).find("textarea").val("");
     $(this).find("input[type=file]").replaceWith($(this).find("input[type=file]").val('').clone(true));
-
     $.ajax({
       url: url,
       type: "POST",
@@ -35,4 +33,29 @@ $(function() {
       alert('送信に失敗しました');
     })
   })
+    var interval = setInterval(function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    $.ajax({
+      url: location.href.json,
+      dataType:'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(json) {
+      var id = $(".main-contents__chat-list ul .chat:last").data('id');
+      var insertHTML = '';
+      json.messages.forEach(function(message) {
+        if (message.id > id ) {
+          insertHTML += buildHTML(message);
+          $('.main-contents__chat-list').animate({scrollTop: $('.main-contents__chat-list').get(0).scrollHeight}, 500);
+        }
+      });
+      $('.main-contents__chat-list ul').append(insertHTML);
+    })
+    .fail(function(json) {
+      alert('自動更新に失敗しました');
+    });
+  } else {
+    clearInterval(interval);
+   }} , 5 * 1000 );
 });
