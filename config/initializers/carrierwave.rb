a05013640cf33956b5c0839a1b04dbe6f4538d14
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-26 17:12:23 +0900

    Revise_carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,12 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
+    region: 'ap-northeast-1',
+    host: 's3.example.com',
+    endpoint: 'https://s3.example.com:8080'
   }
 
   config.fog_directory  = 'update-image'
+  config.fog_public     = false
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/update-image'
 end
